7a8a4931f9a6e05aaff02bb77fb957dd854467d3
Saliou Seck <saliouseck2009@gmail.com>
2021-01-12 22:18:08 +0000

    intégration de ajax

--- src/Controller/BookDetailController.php
+++ src/Controller/BookDetailController.php
@@ -14,35 +14,39 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class BookDetailController extends AbstractController
 {
-//    /**
-//     * @Route("/book{id}", name="book")
-//     */
-//    public function index($id, BookRepository $bookRepository): Response
-//    {
-//        $book = $bookRepository->find($id);
-//        return $this->render('pages/book_detail.html.twig', [
-//            'book'=>$book,
-//        ]);
-//    }
+
+
 
     /**
-     * @Route("/book/{id}", name="book")
+     * @Route("/addbook/{id}", name="addbook")
      * @param MessageRepository $repository
      */
     public function addBook($id,EntityManagerInterface $entity, MessageRepository $messageRepository,BookRepository $bookRepository,UserRepository $userRepository, FlashyNotifier $notifier)
     {
         $book = $bookRepository->find($id);
+        $user=$userRepository->find($this->getUser()->getId());
         if ($this->getUser() == null) {
             $notifier->warning('Veillez vous connecter dabort', $this->generateUrl('security_login'));
             return $this->render('security/login.html.twig');
         } else {
-            $book->addUser($this->getUser());
-            $entity->persist($book);
+
+            if($this->isUserHaveBook($id, $user->getBooks())){
+                return $this->json([
+                    'message'=>"Cette oeuvre existe déjà dans votre liste de livre dans votre liste",
+                    'type'=>"warn"
+                ],200);
+            }
+
+            $user->addBook($book);
             $entity->flush();
-            $recentbook = $bookRepository->findTenMostRecentBook();
-            return $this->render('pages/book-detail.html.twig',[
-                'book'=>$book,
-                'recentbook'=>$recentbook,]);
+            //$recentbook = $bookRepository->findTenMostRecentBook();
+            return $this->json([
+                'message'=>"Bravo le livre a été bien enrégistrer dans votre liste de livres",
+                'type'=>"success"
+            ],200);
+//            return $this->render('pages/book-detail.html.twig',[
+//                'book'=>$book,
+//                'recentbook'=>$recentbook,]);
 //            if ($users == null){
 //                $book->addUser($this->getUser());
 //                $notifier->success('Ajouté avec succés', '#');
@@ -53,15 +57,13 @@ class BookDetailController extends AbstractController
     }
 
     /**
-     * @Route("/addbook/{id}", name="addbook")
+     * @Route("/book/{id}", name="book")
      * @param MessageRepository $repository
      */
     public function Book($id,EntityManagerInterface $entity, MessageRepository $messageRepository,BookRepository $bookRepository,UserRepository $userRepository, FlashyNotifier $notifier)
     {
         $book = $bookRepository->find($id);
         $recentbook = $bookRepository->findTenMostRecentBook();
-
-
 
         return $this->render('pages/book-detail.html.twig',[
             'recentbook'=>$recentbook,
@@ -77,6 +79,12 @@ class BookDetailController extends AbstractController
     public function askBook($id, BookRepository $bookRepository,UserRepository $userRepository, MessageRepository $messageRepository, FlashyNotifier $flashyNotifier, EntityManagerInterface $entityManager){
         $book = $bookRepository->find($id);
         $recentbook = $bookRepository->findTenMostRecentBook();
+        if($this->isUserHaveBook($id, $this->getUser()->getBooks())){
+            return $this->json([
+                'message'=>"Cette oeuvre existe déjà dans votre liste de livre dans votre liste",
+                'type'=>"warn"
+            ],200);
+        }
         if ($this->getUser()){
             $users = $userRepository->findUserBook($id);
             if($users) {
@@ -99,17 +107,26 @@ class BookDetailController extends AbstractController
                     }
                 }
 
-                return $this->render('pages/book-detail.html.twig',[
-                    'recentbook'=>$recentbook,
-                    'book'=>$book,
-                ]);
+                return $this->json([
+                    'message'=>'Vous avez déja ce livre en possesion',
+                    'type'=>'warn'
+                ],200);
+//                return $this->render('pages/book-detail.html.twig',[
+//                    'recentbook'=>$recentbook,
+//                    'book'=>$book,
+//                ]);
             }
             else{
                 $flashyNotifier->primary('Ce livre  n\' est pas encore disponible');
-                return $this->render('pages/book-detail.html.twig',[
-                    'recentbook'=>$recentbook,
-                    'book'=>$book,
-                ]);
+
+                return $this->json([
+                    'message'=>'Ce livre  n\' est pas encore disponible',
+                    'type'=>'info'
+                ],200);
+//                return $this->render('pages/book-detail.html.twig',[
+//                    'recentbook'=>$recentbook,
+//                    'book'=>$book,
+//                ]);
             }
 
 
@@ -120,5 +137,18 @@ class BookDetailController extends AbstractController
 
 
     }
+
+
+
+    public function isUserHaveBook($id,$Books):bool{
+        foreach ($Books as $book){
+            if ($book->getId() == $id){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -226,4 +226,6 @@ class User implements UserInterface
 
         return $this;
     }
+
+
 }
